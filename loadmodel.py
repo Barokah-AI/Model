0e98fedfda747ac0098dd02d22b1c0ba5d931d74
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-18 19:33:49 +0700

    Refactor loadmodel.py

--- loadmodel.py
+++ loadmodel.py
@@ -8,11 +8,11 @@ df = pd.read_csv("dataset/13002-14001.csv", sep="|")
 
 # Encode labels
 df['label'] = df['answer'].astype('category').cat.codes
-# label_dict = dict(enumerate(df['answer'].astype('category').cat.categories))
+label_dict = dict(enumerate(df['answer'].astype('category').cat.categories))
 
 
-# tokenizer = BertTokenizer.from_pretrained("./models/model v"+str(version))
-# model = BertForSequenceClassification.from_pretrained("./models/model v"+str(version))
+tokenizer = BertTokenizer.from_pretrained("./models/model v"+str(version))
+model = BertForSequenceClassification.from_pretrained("./models/model v"+str(version))
 
 
 def get_answer(question):
