5e9cb272ce37719b730b9f5c4fe22f221c4cfae6
stefanymelda <juwitastefany13@gmail.com>
2024-07-18 19:29:45 +0700

    update loadmodel

--- loadmodel.py
+++ loadmodel.py
@@ -7,8 +7,8 @@ version = 6
 df = pd.read_csv("dataset/13002-14001.csv", sep="|")
 
 # Encode labels
-df['label'] = df['answer'].astype('category').cat.codes
-label_dict = dict(enumerate(df['answer'].astype('category').cat.categories))
+# df['label'] = df['answer'].astype('category').cat.codes
+# label_dict = dict(enumerate(df['answer'].astype('category').cat.categories))
 
 
 tokenizer = BertTokenizer.from_pretrained("./models/model v"+str(version))
