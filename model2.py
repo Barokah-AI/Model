30b18d95fb6b2bed0eaf9fdc5e3ba8ff142875a1
stefanymelda <juwitastefany13@gmail.com>
2024-07-18 20:36:15 +0700

    Refactor code model

--- model2.py
+++ model2.py
@@ -48,11 +48,11 @@ training_args = TrainingArguments(
 )
 
 # Create Trainer
-trainer = Trainer(
-    model=model,                         
-    args=training_args,                  
-    train_dataset=dataset,         
-)
+# trainer = Trainer(
+#     model=model,                         
+#     args=training_args,                  
+#     train_dataset=dataset,         
+# )
 
 # Train the model
 trainer.train()
