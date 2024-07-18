a70452365d138187c8ec81c1893b8b64d0171c40
stefanymelda <juwitastefany13@gmail.com>
2024-07-18 19:37:02 +0700

    update loadmodel

--- loadmodel.py
+++ loadmodel.py
@@ -17,11 +17,11 @@ model = BertForSequenceClassification.from_pretrained("./models/model v"+str(ver
 
 def get_answer(question):
     inputs = tokenizer(question, return_tensors="pt")
-    outputs = model(**inputs)
-    answer = torch.argmax(outputs.logits).item()
-    print(max(outputs.logits))
-    print(answer)
-    return label_dict[answer]
+    # outputs = model(**inputs)
+    # answer = torch.argmax(outputs.logits).item()
+    # print(max(outputs.logits))
+    # print(answer)
+    # return label_dict[answer]
     # return outputs
 
 # Test the model
