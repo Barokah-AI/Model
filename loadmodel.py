7263fd528fa4abcbd44c5e1cf01e81b4f224eee5
Agitanurfd <agitanurfadillah45@gmail.com>
2024-07-19 00:00:30 +0700

    update test the model

--- loadmodel.py
+++ loadmodel.py
@@ -26,9 +26,9 @@ def get_answer(question):
 
 # Test the model
 while True:
-    user_input = input("Tanyakan sesuatu atau ketik 'exit' untuk keluar: ")
+    user_input = input("Tanyakan Sesuatu atau Ketik 'Exit' Untuk Keluar: ")
     if user_input.lower() == 'exit':
-        print("Terima kasih dan Sampai jumpa!")
+        print("Terimakasih dan Sampai jumpa!")
         break
     answer = get_answer(user_input)
     print(f"Jawaban: {answer}")
