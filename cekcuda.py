8e2d7b1a597a1e4c3715ed101afaedc4229598a7
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-18 17:51:49 +0700

    Refactor CUDA availability check in cekcuda.py

--- cekcuda.py
+++ cekcuda.py
@@ -1,9 +1,9 @@
 import torch
 
 # Verify CUDA availability and device
-print("CUDA available:", torch.cuda.is_available())
-print("Number of GPUs:", torch.cuda.device_count())
-print("PyTorch version:", torch.__version__)
+# print("CUDA available:", torch.cuda.is_available())
+# print("Number of GPUs:", torch.cuda.device_count())
+# print("PyTorch version:", torch.__version__)
 
 print(torch.version.cuda)
 if torch.cuda.is_available():
