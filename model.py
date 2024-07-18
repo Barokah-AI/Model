826ba57c91ac507a035c8ac3de0f00b35bc19591
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-18 19:28:04 +0700

    Refactor CUDA availability check in model.py

--- model.py
+++ model.py
@@ -22,12 +22,12 @@ def get_next_model_version(models_directory):
     return name_models + str(max_version + 1)
 
 # Verify CUDA availability and device
-# print("CUDA available:", torch.cuda.is_available())
-# print("Number of GPUs:", torch.cuda.device_count())
-# if torch.cuda.is_available():
-#     print("CUDA device name:", torch.cuda.get_device_name(0))
-# else:
-#     print("CUDA not available. Check your CUDA installation and NVIDIA drivers.")
+print("CUDA available:", torch.cuda.is_available())
+print("Number of GPUs:", torch.cuda.device_count())
+if torch.cuda.is_available():
+    print("CUDA device name:", torch.cuda.get_device_name(0))
+else:
+    print("CUDA not available. Check your CUDA installation and NVIDIA drivers.")
 
 # Load dataset
 df = pd.read_csv("dataset/barokah.csv", sep="|")
