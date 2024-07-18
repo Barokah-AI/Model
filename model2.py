6418e0cf85bf534b3a51f9578055fdead82616b3
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-18 19:39:05 +0700

    update QADataset class

--- model2.py
+++ model2.py
@@ -12,25 +12,25 @@ tokenizer = T5Tokenizer.from_pretrained('t5-small')
 inputs = "generate answer: " + df['question'] + " </s>"
 targets = df['answer'] + " </s>"
 
-# class QADataset(torch.utils.data.Dataset):
-#     def __init__(self, inputs, targets, tokenizer, max_length=64):
-#         self.inputs = inputs
-#         self.targets = targets
-#         self.tokenizer = tokenizer
-#         self.max_length = max_length
+class QADataset(torch.utils.data.Dataset):
+    def __init__(self, inputs, targets, tokenizer, max_length=64):
+        self.inputs = inputs
+        self.targets = targets
+        self.tokenizer = tokenizer
+        self.max_length = max_length
 
-#     def __len__(self):
-#         return len(self.inputs)
+    def __len__(self):
+        return len(self.inputs)
 
-#     def __getitem__(self, idx):
-#         input_encodings = self.tokenizer(self.inputs[idx], truncation=True, padding='max_length', max_length=self.max_length, return_tensors="pt")
-#         target_encodings = self.tokenizer(self.targets[idx], truncation=True, padding='max_length', max_length=self.max_length, return_tensors="pt")
+    def __getitem__(self, idx):
+        input_encodings = self.tokenizer(self.inputs[idx], truncation=True, padding='max_length', max_length=self.max_length, return_tensors="pt")
+        target_encodings = self.tokenizer(self.targets[idx], truncation=True, padding='max_length', max_length=self.max_length, return_tensors="pt")
 
-#         input_ids = input_encodings['input_ids'].squeeze()
-#         attention_mask = input_encodings['attention_mask'].squeeze()
-#         labels = target_encodings['input_ids'].squeeze()
+        input_ids = input_encodings['input_ids'].squeeze()
+        attention_mask = input_encodings['attention_mask'].squeeze()
+        labels = target_encodings['input_ids'].squeeze()
         
-#         return {'input_ids': input_ids, 'attention_mask': attention_mask, 'labels': labels}
+        return {'input_ids': input_ids, 'attention_mask': attention_mask, 'labels': labels}
 
 dataset = QADataset(inputs.tolist(), targets.tolist(), tokenizer)
 
