aeb50f9a20897db9811a5b1956fc46780e567121
Agitanurfd <agitanurfadillah45@gmail.com>
2024-07-18 23:59:38 +0700

    add print pada verify CUDA

--- cekcuda.py
+++ cekcuda.py
@@ -4,6 +4,8 @@ import torch
 print("CUDA available:", torch.cuda.is_available())
 print("Number of GPUs:", torch.cuda.device_count())
 print("PyTorch version:", torch.__version__)
+print("PyTorch2 version:", torch.__version__)
+print("PyTorch3 version:", torch.__version__)
 
 print(torch.version.cuda)
 if torch.cuda.is_available():
