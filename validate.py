1224434aeeab2cf926a287c0ef077bc66b0c8dbe
stefanymelda <juwitastefany13@gmail.com>
2024-07-19 11:01:07 +0700

    loading and label encoding

--- validate.py
+++ validate.py
@@ -1,12 +1,12 @@
 import pandas as pd
-# Load dataset
+
 # Load dataset
 df = pd.read_csv("dataset/barokah.csv", sep="|")
 
 # Encode labels
 df['label'] = df['answer'].astype('category').cat.codes
-label_dict = dict(enumerate(df['answer'].astype('category').cat.categories))
-print("Label dictionary:", label_dict)
+# label_dict = dict(enumerate(df['answer'].astype('category').cat.categories))
+# print("Label dictionary:", label_dict)
 
 # Check for invalid labels (-1)
 invalid_labels = df[df['label'] == -1]
