3b214869feb2830d35bc641c1d8cd4738ae030d6
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-19 11:56:49 +0700

    get_next_model_version function for better readability and performance

--- noCudaTrain.py
+++ noCudaTrain.py
@@ -11,15 +11,15 @@ import numpy as np
 models_directory = "./models"
 name_models = "model v"
 
-# def get_next_model_version(models_directory):
-#     contents = os.listdir(models_directory)
-#     max_version = 0
-#     for item in contents:
-#         match = re.search(r'v(\d+)', item)
-#         if match:
-#             number = int(match.group(1))
-#             max_version = max(max_version, number)
-#     return name_models + str(max_version + 1)
+def get_next_model_version(models_directory):
+    contents = os.listdir(models_directory)
+    max_version = 0
+    for item in contents:
+        match = re.search(r'v(\d+)', item)
+        if match:
+            number = int(match.group(1))
+            max_version = max(max_version, number)
+    return name_models + str(max_version + 1)
 
 # Load dataset
 df = pd.read_csv("dataset/barokah-1.csv", sep="|")
