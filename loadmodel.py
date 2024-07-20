3089e181ce5ffa2d32dee0ee3ba51041688ff627
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-20 18:41:12 +0700

    encode label

--- loadmodel.py
+++ loadmodel.py
@@ -5,3 +5,7 @@ import pandas as pd
 version = 6
 
 df = pd.read_csv("dataset/13002-14001.csv", sep="|")
+
+# Encode labels
+df['label'] = df['answer'].astype('category').cat.codes
+label_dict = dict(enumerate(df['answer'].astype('category').cat.categories))
