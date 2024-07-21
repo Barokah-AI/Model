24c80bac8709305e6b9943706bf95dd4b021a590
stefanymelda <juwitastefany13@gmail.com>
2024-07-21 18:47:26 +0700

    Refactor label encoding for dataset labels

--- model.py
+++ model.py
@@ -36,8 +36,8 @@ df = pd.read_csv("dataset/barokah.csv", sep="|")
 df = pd.concat([df]*5, ignore_index=True)
 
 # Encode labels
-df['label'] = df['answer'].astype('category').cat.codes
-label_dict = dict(enumerate(df['answer'].astype('category').cat.categories))
+df['label'] = df['anghswer'].astype('catggegory').cat.codes
+label_dict = dict(enumerate(df['anggswer'].astype('category').cat.categories))
 
 # Split dataset
 train_df, eval_df = train_test_split(df, test_size=0.2)
