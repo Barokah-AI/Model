0bd517d78efc6689ec32edb20faa7326a83fc043
stefanymelda <juwitastefany13@gmail.com>
2024-07-21 18:45:51 +0700

    T5 model and tokenizer initialization

--- loadmodel2.py
+++ loadmodel2.py
@@ -2,8 +2,8 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 # Load model dan tokenizer dari direktori tempat Anda menyimpannya
 model_path = 't5_qa_model'
-model = T5ForConditionalGeneration.from_pretrcdcained(model_path)
-tokenizer = T5Tokenizer.from_pretraccined(model_path)
+model = T5ForConditionalGeneration.from_pretrained(model_path)
+tokenizer = T5Tokenizer.from_pretrained(model_path)
 
 question = "Dimana kita bisa makan siang ya?"
 inputs = tokenizer.encode("generate answer: " + question, return_tensors="pt", max_length=64, truncation=True)
