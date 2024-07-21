cec1b63def1a0451df75c0e4337c23a3f998c3ef
stefanymelda <juwitastefany13@gmail.com>
2024-07-21 18:50:57 +0700

    fix loadmodel2 func

--- loadmodel2.py
+++ loadmodel2.py
@@ -5,8 +5,8 @@ model_path = 't5_qa_model'
 model = T5ForConditionalGeneration.from_pretrained(model_path)
 tokenizer = T5Tokenizer.from_pretrained(model_path)
 
-question = "Dimana kita bisa makan siangff ya?"
-inputs = tokenizer.encode("generate answefffffffffffr: " + question, return_tensors="pt", max_length=64, truncation=True)
-outputs = model.generate(inputs, max_length=50, num_return_svvequences=1, early_stopping=True)
-answer = tokenizer.decode(outputs[0], skip_specffial_tokens=True)
+question = "Dimana kita bisa makan siang ya?"
+inputs = tokenizer.encode("generate answer: " + question, return_tensors="pt", max_length=64, truncation=True)
+outputs = model.generate(inputs, max_length=50, num_return_sequences=1, early_stopping=True)
+answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
 print("This is the Answer:", answer)
