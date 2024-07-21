4d2a66f3f5be1e4650841ecaa7e01b2489c7432d
stefanymelda <juwitastefany13@gmail.com>
2024-07-21 18:37:33 +0700

    CUDA version handling

--- cekcuda.py
+++ cekcuda.py
@@ -5,8 +5,6 @@ print("CUDA available:", torch.cuda.is_available())
 print("Number of GPUs:", torch.cuda.device_count())
 print("PyTorch version:", torch.__version__)
 
+print(torch.version.cuda)
 if torch.cuda.is_available():
-    print("CUDA version:", torch.version.cuda)
     print("CUDA device name:", torch.cuda.get_device_name(0))
-else:
-    print("CUDA is not available.")
