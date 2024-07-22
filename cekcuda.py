fe83560b62e184d62bf5ca74fb557075fb424cd6
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-22 21:42:30 +0700

    Update CUDA

--- cekcuda.py
+++ cekcuda.py
@@ -1,10 +1,10 @@
 import torch
 
 # Verify CUDA availability and device
-print("CUDA available:", torch.cuda.is_available())
-print("Number of GPUs:", torch.cuda.device_count())
-print("PyTorch version:", torch.__version__)
+# print("CUDA available:", torch.cuda.is_available())
+# print("Number of GPUs:", torch.cuda.device_count())
+# print("PyTorch version:", torch.__version__)
 
-print(torch.version.cuda)
-if torch.cuda.is_available():
-    print("CUDA device name:", torch.cuda.get_device_name(0))
+# print(torch.version.cuda)
+# if torch.cuda.is_available():
+#     print("CUDA device name:", torch.cuda.get_device_name(0))
