d62a34cee8c6475a72765433c2b202155d765422
erditona <erditonaushaadam@gmail.com>
2024-07-22 12:20:48 +0700

    tambah deskripsi kode

--- loadmodel.py
+++ loadmodel.py
@@ -1,20 +1,23 @@
+# Description: Load the model and tokenizer from the specified version and test it with user input
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
 import pandas as pd
 
+# Load the model and tokenizer
 version = 6
 
+# Load dataset
 df = pd.read_csv("dataset/13002-14001.csv", sep="|")
 
 # Encode labels
 df['label'] = df['answer'].astype('category').cat.codes
 label_dict = dict(enumerate(df['answer'].astype('category').cat.categories))
 
-
+# Load tokenizer and model
 tokenizer = BertTokenizer.from_pretrained("./models/model v"+str(version))
 model = BertForSequenceClassification.from_pretrained("./models/model v"+str(version))
 
-
+# Tokenize dataset and include labels
 def get_answer(question):
     inputs = tokenizer(question, return_tensors="pt")
     outputs = model(**inputs)
