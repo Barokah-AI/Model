9170080e7dd2b913f559aaa31b0375a42e899364
erditona <erditonaushaadam@gmail.com>
2024-07-22 12:15:07 +0700

    updaet cekcuda.py

--- cekcuda.py
+++ cekcuda.py
@@ -1,13 +1,13 @@
-# Cek apakah CUDA tersedia dan informasi perangkat CUDA yang digunakan
+# # Cek apakah CUDA tersedia dan informasi perangkat CUDA yang digunakan
 
-import torch
+# import torch
 
-# Verify CUDA availability and device
-print("CUDA available:", torch.cuda.is_available())
-print("Number of GPUs:", torch.cuda.device_count())
-print("PyTorch version:", torch.__version__)
+# # Verify CUDA availability and device
+# print("CUDA available:", torch.cuda.is_available())
+# print("Number of GPUs:", torch.cuda.device_count())
+# print("PyTorch version:", torch.__version__)
 
-# Print CUDA version and device name
-print(torch.version.cuda)
-if torch.cuda.is_available():
-    print("CUDA device name:", torch.cuda.get_device_name(0))
+# # Print CUDA version and device name
+# print(torch.version.cuda)
+# if torch.cuda.is_available():
+#     print("CUDA device name:", torch.cuda.get_device_name(0))
