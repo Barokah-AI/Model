ef5c14848b7126ddf9c4f48a46a6a780c5522b83
AdeCandra12 <ade.can321@gmail.com>
2024-07-22 16:45:42 +0700

    Update Model

--- model.py
+++ model.py
@@ -55,3 +55,6 @@ def preprocess_function(examples):
     inputs = tokenizer(examples['question'], truncation=True, padding='max_length', max_length=128)
     inputs['label'] = examples['label']
     return inputs
+
+train_dataset = train_dataset.map(preprocess_function, batched=True)
+eval_dataset = eval_dataset.map(preprocess_function, batched=True)
