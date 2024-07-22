c8a2f84824681b6b5de547a208b36ccfb32c2b1c
erditona <erditonaushaadam@gmail.com>
2024-07-22 12:14:36 +0700

    Print CUda version and device name

--- cekcuda.py
+++ cekcuda.py
@@ -7,3 +7,7 @@ print("CUDA available:", torch.cuda.is_available())
 print("Number of GPUs:", torch.cuda.device_count())
 print("PyTorch version:", torch.__version__)
 
+# Print CUDA version and device name
+print(torch.version.cuda)
+if torch.cuda.is_available():
+    print("CUDA device name:", torch.cuda.get_device_name(0))
