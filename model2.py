d5c5b2a4991597150786973a73313bcbbbf108d5
MarlinaLubis <lubismarlina80@gmail.com>
2024-07-22 21:41:08 +0700

    model training code

--- model2.py
+++ model2.py
@@ -37,22 +37,22 @@ dataset = QADataset(inputs.tolist(), targets.tolist(), tokenizer)
 # Load model
 model = T5ForConditionalGeneration.from_pretrained('t5-small')
 
-# Define training arguments
-training_args = TrainingArguments(
-    output_dir='./results',         
-    num_train_epochs=3,              
-    per_device_train_batch_size=2,  
-    warmup_steps=500,                
-    weight_decay=0.01,               
-    logging_dir='./logs',            
-)
+# # Define training arguments
+# training_args = TrainingArguments(
+#     output_dir='./results',         
+#     num_train_epochs=3,              
+#     per_device_train_batch_size=2,  
+#     warmup_steps=500,                
+#     weight_decay=0.01,               
+#     logging_dir='./logs',            
+# )
 
-# Create Trainer
-trainer = Trainer(
-    model=model,                         
-    args=training_args,                  
-    train_dataset=dataset,         
-)
+# # Create Trainer
+# trainer = Trainer(
+#     model=model,                         
+#     args=training_args,                  
+#     train_dataset=dataset,         
+# )
 
 # Train the model
 trainer.train()
