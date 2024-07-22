8ef078d68d6c6bd5359827b1ce1ef8ff83783d68
AdeCandra12 <ade.can321@gmail.com>
2024-07-22 16:18:06 +0700

    Add CUDA availability check in

--- model.py
+++ model.py
@@ -20,3 +20,6 @@ def get_next_model_version(models_directory):
             number = int(match.group(1))
             max_version = max(max_version, number)
     return name_models + str(max_version + 1)
+
+# Verify CUDA availability and device
+print("CUDA available:", torch.cuda.is_available())
