cbb088d3284b3bacf4111f45e9a1894e29362dd7
AdeCandra12 <ade.can321@gmail.com>
2024-07-22 16:05:07 +0700

    refactor: Add user input loop and exit functionality in loadmodel2.py

--- loadmodel2.py
+++ loadmodel2.py
@@ -4,3 +4,5 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 model_path = 't5_qa_model'
 model = T5ForConditionalGeneration.from_pretrained(model_path)
 tokenizer = T5Tokenizer.from_pretrained(model_path)
+
+question = "Dimana kita bisa makan siang?"
