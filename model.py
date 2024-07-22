59e04375774de40083098f7185fd9d97a42e1282
erditona <erditonaushaadam@gmail.com>
2024-07-22 12:00:16 +0700

    update model.py

--- model.py
+++ model.py
@@ -3,4 +3,6 @@ from sklearn.model_selection import train_test_split
 from datasets import Dataset
 from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
 import torch
+import os
+import re
 
