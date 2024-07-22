f6c659598d02c63639ce17af26a7cbfbed6c1113
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-22 14:59:18 +0700

    add import 2

--- model.py
+++ model.py
@@ -1,2 +1,4 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from datasets import Dataset
+from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
