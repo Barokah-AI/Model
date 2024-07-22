6faea58c2a8d3b54252dd8cc64ce48297b620407
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-22 15:03:17 +0700

    load dataset

--- model.py
+++ model.py
@@ -28,3 +28,6 @@ if torch.cuda.is_available():
     print("CUDA device name:", torch.cuda.get_device_name(0))
 else:
     print("CUDA not available. Check your CUDA installation and NVIDIA drivers.")
+
+# Load dataset
+df = pd.read_csv("dataset/barokah.csv", sep="|")
