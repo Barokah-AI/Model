528c92fc6761c6ac21558814ff23d6989fb73e1b
AdeCandra12 <ade.can321@gmail.com>
2024-07-22 17:17:42 +0700

    Update model and add Trainer class, and import necessary dependencies

--- noCudaTrain.py
+++ noCudaTrain.py
@@ -1,3 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from datasets import Dataset
+from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
+import torch
+import os
+import re
+import evaluate
+import numpy as np
