47557c09ed06187ba5041cf7374f91ebb0669173
erditona <erditonaushaadam@gmail.com>
2024-07-22 12:18:00 +0700

    encode label

--- loadmodel.py
+++ loadmodel.py
@@ -6,4 +6,9 @@ version = 6
 
 df = pd.read_csv("dataset/13002-14001.csv", sep="|")
 
+# Encode labels
+df['label'] = df['answer'].astype('category').cat.codes
+label_dict = dict(enumerate(df['answer'].astype('category').cat.categories))
+
+
 
