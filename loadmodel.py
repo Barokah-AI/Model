5d03f5f7f4149f5203aaf79af9c046667fa5d8a5
AdeCandra12 <ade.can321@gmail.com>
2024-07-22 15:56:10 +0700

    refactor: Add BERT tokenizer for sequence classification

--- loadmodel.py
+++ loadmodel.py
@@ -9,3 +9,6 @@ df = pd.read_csv("dataset/13002-14001.csv", sep="|")
 # Encode labels
 df['label'] = df['answer'].astype('category').cat.codes
 label_dict = dict(enumerate(df['answer'].astype('category').cat.categories))
+
+
+tokenizer = BertTokenizer.from_pretrained("./models/model v"+str(version))
