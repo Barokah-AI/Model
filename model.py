e5326204e74cddf130ce8dce72ce6bec2f646eac
AdeCandra12 <ade.can321@gmail.com>
2024-07-22 16:44:12 +0700

    Update model

--- model.py
+++ model.py
@@ -49,3 +49,4 @@ eval_dataset = Dataset.from_pandas(eval_df)
 # Load tokenizer and model
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=len(df['label'].unique()))
+
