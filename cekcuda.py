eef7495e81caa018253906fce7e16873e23ad5d3
AdeCandra12 <ade.can321@gmail.com>
2024-07-22 15:50:49 +0700

    refactor: Update CUDA availability and device check

--- cekcuda.py
+++ cekcuda.py
@@ -4,3 +4,7 @@ import torch
 print("CUDA available:", torch.cuda.is_available())
 print("Number of GPUs:", torch.cuda.device_count())
 print("PyTorch version:", torch.__version__)
+
+print(torch.version.cuda)
+if torch.cuda.is_available():
+    print("CUDA device name:", torch.cuda.get_device_name(0))
