5476b65ac9f385ea4b89ad6ad6372484ee4893cc
erditona <erditonaushaadam@gmail.com>
2024-07-22 13:45:30 +0700

    Load dataset in training.py

--- training.py
+++ training.py
@@ -5,3 +5,6 @@ import re
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from sklearn.model_selection import train_test_split
 
+# Load the dataset
+df = pd.read_csv('questions.csv', sep='|', usecols=['question', 'answer'])
+
