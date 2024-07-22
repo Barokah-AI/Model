2a2293e4e50a3694b36249c979aa43b92216d7d9
erditona <erditonaushaadam@gmail.com>
2024-07-22 13:45:43 +0700

    feat: Add preprocessing functions to training.py

--- training.py
+++ training.py
@@ -8,3 +8,9 @@ from sklearn.model_selection import train_test_split
 # Load the dataset
 df = pd.read_csv('questions.csv', sep='|', usecols=['question', 'answer'])
 
+# Preprocessing functions
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
+
+punct_re_escape = re.compile('[%s]' % re.escape('!"#$%&()*+,./:;<=>?@[\\]^_`{|}~'))
+
