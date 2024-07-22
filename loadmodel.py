2a36bc2bb30c65da50c101029dbd8281282b877e
erditona <erditonaushaadam@gmail.com>
2024-07-22 12:22:55 +0700

    update loadmodel.py

--- loadmodel.py
+++ loadmodel.py
@@ -1,37 +1,37 @@
-# Description: Load the model and tokenizer from the specified version and test it with user input
-from transformers import BertTokenizer, BertForSequenceClassification
-import torch
-import pandas as pd
+# # Description: Load the model and tokenizer from the specified version and test it with user input
+# from transformers import BertTokenizer, BertForSequenceClassification
+# import torch
+# import pandas as pd
 
-# Load the model and tokenizer
-version = 6
+# # Load the model and tokenizer
+# version = 6
 
-# Load dataset
-df = pd.read_csv("dataset/13002-14001.csv", sep="|")
+# # Load dataset
+# df = pd.read_csv("dataset/13002-14001.csv", sep="|")
 
-# Encode labels
-df['label'] = df['answer'].astype('category').cat.codes
-label_dict = dict(enumerate(df['answer'].astype('category').cat.categories))
+# # Encode labels
+# df['label'] = df['answer'].astype('category').cat.codes
+# label_dict = dict(enumerate(df['answer'].astype('category').cat.categories))
 
-# Load tokenizer and model
-tokenizer = BertTokenizer.from_pretrained("./models/model v"+str(version))
-model = BertForSequenceClassification.from_pretrained("./models/model v"+str(version))
+# # Load tokenizer and model
+# tokenizer = BertTokenizer.from_pretrained("./models/model v"+str(version))
+# model = BertForSequenceClassification.from_pretrained("./models/model v"+str(version))
 
-# Tokenize dataset and include labels
-def get_answer(question):
-    inputs = tokenizer(question, return_tensors="pt")
-    outputs = model(**inputs)
-    answer = torch.argmax(outputs.logits).item()
-    print(max(outputs.logits))
-    print(answer)
-    return label_dict[answer]
-    # return outputs
+# # Tokenize dataset and include labels
+# def get_answer(question):
+#     inputs = tokenizer(question, return_tensors="pt")
+#     outputs = model(**inputs)
+#     answer = torch.argmax(outputs.logits).item()
+#     print(max(outputs.logits))
+#     print(answer)
+#     return label_dict[answer]
+#     # return outputs
 
-# Test the model
-while True:
-    user_input = input("Tanyakan sesuatu (atau ketik 'exit' untuk keluar): ")
-    if user_input.lower() == 'exit':
-        print("Terima kasih! Sampai jumpa!")
-        break
-    answer = get_answer(user_input)
-    print(f"Jawaban: {answer}")
+# # Test the model
+# while True:
+#     user_input = input("Tanyakan sesuatu (atau ketik 'exit' untuk keluar): ")
+#     if user_input.lower() == 'exit':
+#         print("Terima kasih! Sampai jumpa!")
+#         break
+#     answer = get_answer(user_input)
+#     print(f"Jawaban: {answer}")
