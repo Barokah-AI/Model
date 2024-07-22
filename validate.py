d1f32847c8c36f986d708e4dbf95b27a71105263
erditona <erditonaushaadam@gmail.com>
2024-07-22 12:23:40 +0700

    encode labels

--- validate.py
+++ validate.py
@@ -3,3 +3,8 @@ import pandas as pd
 # Load dataset
 df = pd.read_csv("dataset/barokah.csv", sep="|")
 
+# Encode labels
+df['label'] = df['answer'].astype('category').cat.codes
+label_dict = dict(enumerate(df['answer'].astype('category').cat.categories))
+print("Label dictionary:", label_dict)
+
