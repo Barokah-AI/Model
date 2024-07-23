185d58f92442bfe85c00d3cda6b38e17115582cc
Agitanurfd <agitanurfadillah45@gmail.com>
2024-07-23 21:11:05 +0700

    add class dataset

--- model2.py
+++ model2.py
@@ -9,19 +9,29 @@ df = pd.read_csv('dataset/questions.csv', delimiter='|', names=['question', 'ans
 tokenizer = T5Tokenizer.from_pretrained('t5-small')
 
 # Combine question and answer into a single string for training
-inputs = "generate answer: " + df['question'] + " </s>"
-targets = df['answer'] + " </s>"
+inputs = "generate answer: " + df['Question'] + " </s>"
+targets = df['Answer'] + " </s>"
 
 class QADataset(torch.utils.data.Dataset):
-    def __init__(self, inputs, targets, tokenizer, max_length=64):
-        self.inputs = inputs
-        self.targets = targets
-        self.tokenizer = tokenizer
-        self.max_length = max_length
+    def __init__(self, Inputs, Targets, Tokenizer, Max_length=64):
+        self.inputs = Inputs
+        self.targets = Targets
+        self.tokenizer = Tokenizer
+        self.max_length = Max_length
 
     def __len__(self):
         return len(self.inputs)
 
+    def __getitem__(self, idx):
+        input_encodings = self.tokenizer(self.inputs[idx], truncation=True, padding='max_length', max_length=self.max_length, return_tensors="pt")
+        target_encodings = self.tokenizer(self.targets[idx], truncation=True, padding='max_length', max_length=self.max_length, return_tensors="pt")
+
+        input_ids = input_encodings['input_ids'].squeeze()
+        attention_mask = input_encodings['attention_mask'].squeeze()
+        labels = target_encodings['input_ids'].squeeze()
+        
+        return {'input_ids': input_ids, 'attention_mask': attention_mask, 'labels': labels}
+    
     def __getitem__(self, idx):
         input_encodings = self.tokenizer(self.inputs[idx], truncation=True, padding='max_length', max_length=self.max_length, return_tensors="pt")
         target_encodings = self.tokenizer(self.targets[idx], truncation=True, padding='max_length', max_length=self.max_length, return_tensors="pt")
