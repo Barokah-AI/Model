d0025810aa3e8b76a12c1f22cb2d47d1e17a6b2b
AdeCandra12 <ade.can321@gmail.com>
2024-07-23 10:58:08 +0700

    Set PyTorch format for train and eval datasets

--- noCudaTrain.py
+++ noCudaTrain.py
@@ -48,3 +48,6 @@ def preprocess_function(examples):
 train_dataset = train_dataset.map(preprocess_function, batched=True)
 eval_dataset = eval_dataset.map(preprocess_function, batched=True)
 
+# Set format for PyTorch
+train_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'label'])
+eval_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'label'])
