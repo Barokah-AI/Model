a08a716fe0f46f51f1f122f3d6641680e8eb8812
Agitanurfd <agitanurfadillah45@gmail.com>
2024-07-23 23:15:21 +0700

    add komentar pada cuda

--- cekcuda.py
+++ cekcuda.py
@@ -5,6 +5,10 @@ print("CUDA available:", torch.cuda.is_available())
 print("Number of GPUs:", torch.cuda.device_count())
 print("PyTorch version:", torch.__version__)
 
+# Mencetak versi CUDA yang tersedia di PyTorch
 print(torch.version.cuda)
+
+# Mengecek apakah CUDA tersedia di perangkat
 if torch.cuda.is_available():
+    # Mencetak nama perangkat CUDA (GPU) yang sedang digunakan
     print("CUDA device name:", torch.cuda.get_device_name(0))
