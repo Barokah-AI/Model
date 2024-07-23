59faab33b971894604e9254b7e32599236e4f9ab
Agitanurfd <agitanurfadillah45@gmail.com>
2024-07-23 22:45:03 +0700

    update load model

--- loadmodel2.py
+++ loadmodel2.py
@@ -5,6 +5,7 @@ model_path = 't5_qa_model'
 model = T5ForConditionalGeneration.from_pretrained(model_path)
 tokenizer = T5Tokenizer.from_pretrained(model_path)
 
+question = "Dimana kita bisa makan siang?"
 inputs = tokenizer.encode("generate answer: " + question, return_tensors="pt", max_length=64, truncation=True)
 outputs = model.generate(inputs, max_length=50, num_return_sequences=1, early_stopping=True)
 answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
