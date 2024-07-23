6db016ec9bbb6bdb85402be5685609707f22a472
Agitanurfd <agitanurfadillah45@gmail.com>
2024-07-23 20:39:14 +0700

    update print pada torch cuda

--- cekcuda.py
+++ cekcuda.py
@@ -3,9 +3,8 @@ import torch
 # Verify CUDA availability and device
 print("CUDA available:", torch.cuda.is_available())
 print("Number of GPUs:", torch.cuda.device_count())
-# print("PyTorch version:", torch.__version__)
 print("PyTorch version:", torch.__version__)
 
 print(torch.version.cuda)
 if torch.cuda.is_available():
-    print("CUDA Device Name:", torch.cuda.get_device_name(0))
+    print("CUDA device name:", torch.cuda.get_device_name(0))
