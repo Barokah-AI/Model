1e7fe30f56f35c82e4c6eec3110fdd5a2d9c282c
dani ferdinan <daniferdinandall@gmail.com>
2024-07-25 18:03:37 +0700

    update model

--- model.py
+++ model.py
@@ -111,7 +111,7 @@ metric = evaluate.load("accuracy", trust_remote_code=True)
 def compute_metrics(eval_pred):
   logits, labels = eval_pred
   if isinstance(logits, np.ndarray):
-      logits = torch.tensor(logits)
+    logits = torch.tensor(logits)
   if isinstance(labels, np.ndarray):
       labels = torch.tensor(labels)
   predictions = torch.argmax(logits, dim=-1)
