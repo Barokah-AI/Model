bc24e22d20d12732f5d8ec6301b18fc63de6eab2
dani ferdinan <daniferdinandall@gmail.com>
2024-07-25 18:08:34 +0700

    update laodmodel

--- loadmodel.py
+++ loadmodel.py
@@ -2,25 +2,26 @@ from transformers import BertTokenizer, BertForSequenceClassification
 import torch
 import pandas as pd
 
-version = 6
+version = 12
 
-df = pd.read_csv("dataset/13002-14001.csv", sep="|")
+df = pd.read_csv("dataset/barokah.csv", sep="|")
 
 # Encode labels
 df['label'] = df['answer'].astype('category').cat.codes
 label_dict = dict(enumerate(df['answer'].astype('category').cat.categories))
 
 
-tokenizer = BertTokenizer.from_pretrained("./models/model v"+str(version))
+# tokenizer = BertTokenizer.from_pretrained("./models/model v"+str(version))
 model = BertForSequenceClassification.from_pretrained("./models/model v"+str(version))
 
 
 def get_answer(question):
-    inputs = tokenizer(question, return_tensors="pt")
+    # inputs = tokenizer(question, return_tensors="pt")
+    inputs = question
     outputs = model(**inputs)
     answer = torch.argmax(outputs.logits).item()
-    print(max(outputs.logits))
-    print(answer)
+    # print(max(outputs.logits))
+    # print(answer)
     return label_dict[answer]
     # return outputs
 
