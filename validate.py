6945cb6cabe2b68323be719e8079b39a6976beed
dani ferdinan <daniferdinandall@gmail.com>
2024-07-25 17:49:22 +0700

    update validate model

--- validate.py
+++ validate.py
@@ -1,7 +1,7 @@
 import pandas as pd
 # Load dataset
 # Load dataset
-df = pd.read_csv("dataset/barokah.csv", sep="|")
+df = pd.read_csv("dataset/barokah-1.csv", sep="|")
 
 # Encode labels
 df['label'] = df['answer'].astype('category').cat.codes
