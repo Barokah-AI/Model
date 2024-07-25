6a23864285fb29c6c9bab816332021ae497a987d
dimasardnt6 <dimasardnt6@gmail.com>
2024-07-25 18:57:29 +0700

    load dataset

--- model2.py
+++ model2.py
@@ -1,3 +1,6 @@
 import pandas as pd
 from transformers import T5Tokenizer, T5ForConditionalGeneration, Trainer, TrainingArguments
 import torch
+
+# Load the dataset
+df = pd.read_csv('dataset/questions.csv', delimiter='|', names=['question', 'answer'])
