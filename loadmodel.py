6c7c84246d9b21f2167884c71cfd01f29753c1ec
dani ferdinan <daniferdinandall@gmail.com>
2024-07-25 18:12:22 +0700

    update load model

--- loadmodel.py
+++ loadmodel.py
@@ -1,6 +1,6 @@
-from transformers import BertTokenizer, BertForSequenceClassification
 import torch
 import pandas as pd
+from transformers import BertTokenizer, BertForSequenceClassification
 
 version = 12
 
