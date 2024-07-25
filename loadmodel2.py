1bb670f0813b6e8fc2f09a0f6d65133dd2453eb1
dani ferdinan <daniferdinandall@gmail.com>
2024-07-25 18:18:24 +0700

    update load model 2

--- loadmodel2.py
+++ loadmodel2.py
@@ -4,6 +4,7 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 # Load model dan tokenizer dari direktori tempat Anda menyimpannya
 model_path = 't5_qa_model'
+#  Load model dari direktori yang sudah di-training sebelumnya
 model = T5ForConditionalGeneration.from_pretrained(model_path)
 tokenizer = T5Tokenizer.from_pretrained(model_path)
 
